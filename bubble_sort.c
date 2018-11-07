0bab763adc80f838c3c4b5d9bcca5a36be15e4cb
Alexander <alexanderkoltygin@gmail.com>
2018-11-07 18:34:47 +0600

    Bug fix

--- bubble_sort.c
+++ bubble_sort.c
@@ -4,7 +4,7 @@ void bubbleSort(int arr[], int length)
 {
 	for (int i = 0; i < length; i++)
 	{
-		for (int j = 0; j < *length - i - 1; j++)
+		for (int j = 0; j < length - i - 1; j++)
 		{
 			if (arr[j] > arr[j + 1]) {
 				float tmp = arr[j + 1];
